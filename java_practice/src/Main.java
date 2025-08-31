784c03c078acaf3d2b023691100adfa9f2ae92a3
Rakesh <rakeshkraki3272@gmail.con>
2025-08-31 14:21:56 +0530

    ternary operator

---
+++ java_practice/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ternaryOperator(35);
+        nestedTernay(10);
+    }
+
+    public static void ternaryOperator(int marks){
+        String result = (marks > 35) ? "pass" : "fail";
+        System.out.println(result);
+    }
+
+    public static void nestedTernay(int temperature) {
+        String message = (temperature < 0) ? "It's freezing!" : (temperature < 20) ? "It's cool." : "It's warm.";
+        System.out.println(message);
+
+    }
+}
