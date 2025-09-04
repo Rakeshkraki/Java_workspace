6af2e965ebe1b502ad779598c6a79fcde0aa6a00
Rakesh K <rakeshkraki3272@gmail.com>
2025-09-04 20:50:54 +0530

    Encapsulation Example

---
+++ java_practice/src/com/rakesh/Encapsulation/Person.java
@@ -0,0 +1,15 @@
+package com.rakesh.Encapsulation;
+
+public class Person {
+
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+}
