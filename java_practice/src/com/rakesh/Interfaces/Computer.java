53d942f3ebbbd760eab77650f9994e8e43487a27
Rakesh K <rakeshkraki3272@gmail.com>
2025-09-09 21:11:23 +0530

    implementation

--- java_practice/src/com/rakesh/Interfaces/Computer.java
+++ java_practice/src/com/rakesh/Interfaces/Computer.java
@@ -1,7 +1,18 @@
 package com.rakesh.Interfaces;
 
+import java.util.SortedMap;
+
 interface Computer {
 
     void display();
     void keyboard();
+
+    private void powerOn(){
+        System.out.println("Loading....");
+    }
+
+
+    public static void setPassword(){
+        System.out.println("this is static method");
+    }
 }
