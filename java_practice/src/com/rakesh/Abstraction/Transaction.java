199cabea949499f789b79e5d908b398aa2ab52ee
Rakesh K <rakeshkraki3272@gmail.com>
2025-09-10 22:58:01 +0530

    payment Example

--- java_practice/src/com/rakesh/Abstraction/Transaction.java
+++ java_practice/src/com/rakesh/Abstraction/Transaction.java
@@ -1,4 +1,14 @@
 package com.rakesh.Abstraction;
 
 public class Transaction {
+
+    public static void main(String[] args) {
+        PaymentProcessor processor = new PaymentProcessor();
+
+        Payment transaction1 = new CreditCardPayment();
+        Payment transaction2 = new UPIPayment();
+
+        processor.makePayment(transaction1,1000d);
+        processor.makePayment(transaction2,200d);
+    }
 }
