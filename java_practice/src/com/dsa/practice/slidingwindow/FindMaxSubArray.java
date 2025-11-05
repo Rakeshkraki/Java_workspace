498cfa0a22075a9fa450f896774513df263dfe27
Rakesh K <rakeshkraki3272@gmail.com>
2025-11-05 20:58:52 +0530

    sliding window prob 1

---
+++ java_practice/src/com/dsa/practice/slidingwindow/FindMaxSubArray.java
@@ -0,0 +1,30 @@
+package com.dsa.practice.slidingwindow;
+
+public class FindMaxSubArray {
+
+
+    public static void main(String[] args){
+
+        System.out.println(findKSubArray(new int[] {3, 8, 7, 8, 7, 5}, 3));
+    }
+
+    public static int findKSubArray(int[] nums, int k){
+        int maxCount = 0;
+        int windowSum = 0;
+
+        // creating first window count
+
+        for(int i = 0; i < k; i++){
+            windowSum += nums[i];
+        }
+
+        maxCount = windowSum;
+
+        for(int i = k; i < nums.length; i++){
+            windowSum += nums[i] - nums[i - k];
+
+            if(windowSum > maxCount) maxCount = windowSum;
+        }
+        return maxCount;
+    }
+}
--- java_practice/src/com/leetcode/practice/PROB331.java
+++ java_practice/src/com/leetcode/practice/PROB331.java
@@ -6,17 +6,24 @@ public class PROB331 {
 
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(findXSum(new int[]{3, 8, 7 ,8, 7, 5}, 2, 2)));
+        System.out.println(Arrays.toString(findXSum(new int[]{3, 8, 7 ,8, 7, 5}, 3, 3)));
 
     }
 
     public static int[] findXSum(int[] nums, int k, int x) {
 
-        int[] newNums = new int[nums.length - 1];
-        if( k == x) {
-            for(int i = 0; i < nums.length - 1; i++){
+        int[] newNums = new int[10];
 
-                newNums[i] = nums[i] + nums[i+1];
+        if( k == x) {
+            for(int i = 0; i < nums.length - k + 1; i++){
+
+                int count = 0;
+                int f = k;
+                while(f > 0){
+                    count += nums[i + f];
+                    f--;
+                }
+                newNums[i] = count;
             }
         }
 
